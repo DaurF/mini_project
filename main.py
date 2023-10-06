06ff8e335a0080eeb042e58338c89012256842f6
gulnaz <gulnaeez@gmail.com>
2023-10-06 16:25:20 +0600

    changes

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import sys
 
 def add(x, y): return x + y
 
+def sub(x, y): return x - y
+
 if __name__ == '__main__':
-    x, y = sys.argv[1], sys.argv[2]
+    x, y = int(sys.argv[1]), int(sys.argv[2])
+    print(x, y)
+    print(add(x, y))
+    print(sub(x, y))
